b6bd83e21bba5caaedcd1cebdaadcd375230bdbc
fecrakib <rakibislam132575@gmail.com>
2023-03-20 17:36:14 +0600

    addTask component desigin done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './component/Header';
 import Fotter from './component/Fotter';
+import AddTask from './component/AddTask';
 const App = () => {
   return (
     <div className='wrapper bg-gradient-to-t from-gray-900 to-teal-900 min-h-screen text-xl text-gray-100 flex-col py-10'>
     <Header/>
+    <AddTask/>
     <Fotter/>
     </div>
   );
--- src/component/AddTask.js
+++ src/component/AddTask.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function AddTask() {
   return (
-    <div>AddTask</div>
+   <form action="" className='bg-gray-900 mx-auto container flex justify-between'>
+    <input type="text" placeholder='what things to do' className='bg-transparent outline-none border-b-2 py-2  px-5 focus:border-teal-600 '/>
+    <button className='px-5 bg-teal-900 py-2 rounded-sm mt-5 border-2 border-teal-200 hover:bg-teal-500 hover:text-gray-900 duration-300' type='submit'>Add task</button>
+   </form>
   )
 }
 
