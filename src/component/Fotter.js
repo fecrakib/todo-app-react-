22100e1b6d23d07f7a42afee434e18542ab07937
fecrakib <rakibislam132575@gmail.com>
2023-03-20 15:09:50 +0600

    footer component

--- src/component/Fotter.js
+++ src/component/Fotter.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Fotter = () => {
     return (
-        <div>
-            
+        <div className='bg-gray-900 container mx-auto text-center p-10 text-sm text-teal-600'>
+           <p>&copy; {new Date().getFullYear()} React Todo App .All rights reserved</p> 
         </div>
     );
 };
